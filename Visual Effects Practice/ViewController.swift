086232d9681cf4382cbf24dc8e990a0108b4a218
Terry Bu <terrybu88@gmail.com>
2019-07-27 20:50:43 -0400

    Initial Commit

---
+++ Visual Effects Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Visual Effects Practice
+//
+//  Created by Terry Bu on 7/27/19.
+//  Copyright © 2019 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
